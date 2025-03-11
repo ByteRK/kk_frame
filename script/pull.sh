cd7315ca29a7d5c04051e15cb328ae30ff51d2d7
ByteRK <75401230+ByteRK@users.noreply.github.com>
2025-03-11 14:06:32 +0800

    Update pull.sh

--- script/pull.sh
+++ script/pull.sh
@@ -2,7 +2,7 @@
  # @Author: Ricken
  # @Email: me@ricken.cn
  # @Date: 2024-07-11 14:18:06
- # @LastEditTime: 2025-01-15 23:42:05
+ # @LastEditTime: 2025-03-10 10:58:41
  # @FilePath: /kk_frame/script/pull.sh
  # @Description: 更新脚本 - 调试用
  # @BugList: 
@@ -13,7 +13,8 @@
 
 NAME=kk_frame
 SPEED=38400
-LIB_DRI=./lib
+APP_DIR=/customer/app
+LIB_DIR=/customer/lib
 IPADDR=192.168.31.3
 SERVEIP=192.168.31.2
 
@@ -25,15 +26,18 @@ ifconfig eth0 $IPADDR up
 for arg in "$@"; do
     case $arg in
         -app)
+            cd $APP_DIR
             tftp -g -r $NAME $SERVEIP -b $SPEED
             chmod +x $NAME
             ;;
         -pak)
+            cd $APP_DIR
             tftp -g -r $NAME.pak  $SERVEIP -b $SPEED
             ;;
         -cdroid)
+            cd $APP_DIR
             tftp -g -r cdroid.pak  $SERVEIP -b $SPEED
-            cd $LIB_DRI
+            cd $LIB_DIR
             tftp -g -r libcdroid.so  $SERVEIP -b $SPEED
             ;;
         *)
