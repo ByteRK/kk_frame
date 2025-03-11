8d9e5d0070a9ae3ed758f240fd35088a21c4d998
ByteRK <75401230+ByteRK@users.noreply.github.com>
2025-03-11 14:07:26 +0800

    Update env.sh

--- script/env.sh
+++ script/env.sh
@@ -3,7 +3,7 @@
  # @Author: Ricken
  # @Email: me@ricken.cn
  # @Date: 2024-06-05 02:05:58
- # @LastEditTime: 2025-01-15 23:04:09
+ # @LastEditTime: 2025-03-11 14:06:56
  # @FilePath: /kk_frame/script/env.sh
  # @Description: 环境配置脚本
  # @BugList: 
@@ -13,7 +13,8 @@
 ### 
 
 export PATH=$PATH:/config/wifi/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/customer/lib:/config/wifi/
+export LD_LIBRARY_PATH=/customer/lib:/config/wifi:$LD_LIBRARY_PATH
 export FONTCONFIG_PATH=/customer/fonts
 export LANG=zh_CN.UTF-8
+export SCREEN_MARGINS=0,0,0,0
 ulimit -c 0
