9161f754f9b998cd63dc7a9bbfd49b6779803f14
Ricken <hackricken@gmail.com>
2025-12-29 17:19:19 +0800

    🎗️ 累加校验

--- src/utils/check_utils.cc
+++ src/utils/check_utils.cc
@@ -2,7 +2,7 @@
  * @Author: Ricken
  * @Email: me@ricken.cn
  * @Date: 2025-12-29 11:52:11
- * @LastEditTime: 2025-12-29 11:54:18
+ * @LastEditTime: 2025-12-29 17:17:05
  * @FilePath: /kk_frame/src/utils/check_utils.cc
  * @Description: 校验相关的一些函数
  * @BugList:
@@ -12,10 +12,3 @@
 **/
 
 #include "check_utils.h"
-
-uint64_t CheckUtils::checkSum(const char* data, uint64_t length) {
-    uint64_t sum = 0;
-    for (int i = 0; i < length; i++)
-        sum += data[i];
-    return sum;
-}
--- src/utils/check_utils.h
+++ src/utils/check_utils.h
@@ -2,13 +2,13 @@
  * @Author: Ricken
  * @Email: me@ricken.cn
  * @Date: 2025-12-29 11:52:07
- * @LastEditTime: 2025-12-29 13:54:18
+ * @LastEditTime: 2025-12-29 17:17:13
  * @FilePath: /kk_frame/src/utils/check_utils.h
  * @Description: 校验相关的一些函数
- * @BugList: 
- * 
- * Copyright (c) 2025 by Ricken, All Rights Reserved. 
- * 
+ * @BugList:
+ *
+ * Copyright (c) 2025 by Ricken, All Rights Reserved.
+ *
 **/
 
 #ifndef __CHECK_UTILS_H__
@@ -19,10 +19,17 @@
 namespace CheckUtils {
 
     /// @brief 计算校验和
+    /// @tparam T 数据类型
     /// @param data 数据
     /// @param length 数据长度
-    /// @return 
-    uint64_t checkSum(const char* data, uint64_t length);
+    /// @return 校验和
+    template <typename T>
+    inline uint64_t checkSum(const T* data, uint64_t length) {
+        uint64_t sum = 0;
+        for (int i = 0; i < length; i++)
+            sum += data[i];
+        return sum;
+    }
 
 } // namespace CheckUtils
 
