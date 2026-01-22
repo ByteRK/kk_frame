d7a5019c3a5444b10e836f666bdad01854623312
Ricken <hackricken@gmail.com>
2026-01-22 23:00:32 +0800

    ✨ Support MultiChoiceG

--- src/app/page/components/button_group.cc
+++ src/app/page/components/button_group.cc
@@ -2,7 +2,7 @@
  * @Author: xialc
  * @Email:
  * @Date: 2026-01-14 16:18:31
- * @LastEditTime: 2026-01-14 16:51:16
+ * @LastEditTime: 2026-01-22 22:47:13
  * @FilePath: /kk_frame/src/app/page/components/button_group.cc
  * @Description: 按键组
  *               SingleChoice:单选  MultiChoice:多选
@@ -13,6 +13,8 @@
 **/
 
 #include "button_group.h"
+
+// ==================== SingleChoiceG 实现 ====================
 
 SingleChoiceG::SingleChoiceG() {
     mLastID = 0;
@@ -53,7 +55,7 @@ void SingleChoiceG::setIntMap(const ButtonINTValues& values) {
 
 /// @brief 设置按键组监听器
 /// @param l 监听
-void SingleChoiceG::setOnClickListener(View::OnClickListener l) {
+void SingleChoiceG::setOnClickListener(OnItemCLickListener l) {
     mOnClickListener = l;
 }
 
@@ -100,8 +102,10 @@ int SingleChoiceG::getInt() {
 /// @brief 获取指定ID的按键
 /// @param id 按键ID
 /// @return 按键
-View* SingleChoiceG::get(int id) {
-    return mButtons[id];
+View* SingleChoiceG::getView(int id) {
+    auto it = mButtons.find(id);
+    if (it != mButtons.end()) return it->second;
+    return nullptr;
 }
 
 /// @brief 获取按键组
@@ -113,6 +117,185 @@ SingleChoiceG::ButtonViews* SingleChoiceG::getButtons() {
 /// @brief 按键点击事件
 /// @param v 按键
 void SingleChoiceG::onButtonClick(View& v) {
+    if (mOnClickListener)mOnClickListener(getID(), v.getId());
     clickView(v.getId());
-    if (mOnClickListener)mOnClickListener(v);
+}
+
+// ==================== MultiChoiceG 实现 ====================
+
+MultiChoiceG::MultiChoiceG() {
+}
+
+MultiChoiceG::~MultiChoiceG() {
+}
+
+/// @brief 添加按键进行托管
+/// @param view 按键
+/// @return true:成功 false:失败
+bool MultiChoiceG::addView(View* view) {
+    if (!view) {
+        LOG(ERROR) << "button is null";
+        return false;
+    }
+    auto it = mButtons.find(view->getId());
+    if (it != mButtons.end()) {
+        LOG(ERROR) << "button id exists. id=" << view->getId();
+        return false;
+    }
+    view->setOnClickListener([this](View& v) { onButtonClick(v); });
+    mButtons.insert(std::make_pair(view->getId(), view));
+    return true;
+}
+
+/// @brief 设置按键组字符串表
+/// @param values 字符串表
+void MultiChoiceG::setStrMap(const ButtonSTRValues& values) {
+    mSTRValues = values;
+}
+
+/// @brief 设置按键组整型表
+/// @param values 整型表
+void MultiChoiceG::setIntMap(const ButtonINTValues& values) {
+    mINTValues = values;
+}
+
+/// @brief 设置按键组监听器
+/// @param l 监听
+void MultiChoiceG::setOnClickListener(OnItemCLickListener l) {
+    mOnClickListener = l;
+}
+
+/// @brief 设置指定ID的选中状态
+/// @param id 控件id
+/// @param selected 是否选中
+/// @return true:成功 false:失败
+bool MultiChoiceG::selectView(int id, bool selected) {
+    auto it = mButtons.find(id);
+    if (it == mButtons.end()) {
+        LOG(ERROR) << "not find button id. ID=" << id;
+        return false;
+    }
+
+    View* view = it->second;
+    bool currentlySelected = (mSelectedIDs.find(id) != mSelectedIDs.end());
+
+    if (selected && !currentlySelected) {
+        mSelectedIDs.insert(id);
+        view->setActivated(true);
+    } else if (!selected && currentlySelected) {
+        mSelectedIDs.erase(id);
+        view->setActivated(false);
+    }
+
+    return true;
+}
+
+/// @brief 切换指定ID的选中状态
+/// @param id 控件id
+/// @return true:成功 false:失败
+bool MultiChoiceG::toggleView(int id) {
+    auto it = mButtons.find(id);
+    if (it == mButtons.end()) {
+        LOG(ERROR) << "not find button id. ID=" << id;
+        return false;
+    }
+
+    View* view = it->second;
+    bool currentlySelected = (mSelectedIDs.find(id) != mSelectedIDs.end());
+
+    if (currentlySelected) {
+        mSelectedIDs.erase(id);
+        if (view->isActivated())
+            view->setActivated(false);
+    } else {
+        mSelectedIDs.insert(id);
+        if (!view->isActivated())
+            view->setActivated(true);
+    }
+
+    return true;
+}
+
+/// @brief 清除所有选中项
+void MultiChoiceG::clearSelection() {
+    for (auto id : mSelectedIDs) {
+        auto it = mButtons.find(id);
+        if (it != mButtons.end())
+            it->second->setActivated(false);
+    }
+    mSelectedIDs.clear();
+}
+
+/// @brief 判断指定ID是否被选中
+/// @param id 控件id
+/// @return true:选中 false:未选中
+bool MultiChoiceG::isSelected(int id) const {
+    return mSelectedIDs.find(id) != mSelectedIDs.end();
+}
+
+/// @brief 获取所有选中项的ID集合
+/// @return 选中项ID集合
+MultiChoiceG::SelectedSet MultiChoiceG::getSelectedIDs() const {
+    return mSelectedIDs;
+}
+
+/// @brief 获取所有选中项对应的字符串
+/// @return 选中项字符串列表
+std::vector<std::string> MultiChoiceG::getSelectedStrings() const {
+    std::vector<std::string> result;
+    for (auto id : mSelectedIDs) {
+        auto it = mSTRValues.find(id);
+        if (it != mSTRValues.end()) {
+            result.push_back(it->second);
+        }
+    }
+    return result;
+}
+
+/// @brief 获取所有选中项对应的整型值
+/// @return 选中项整型值列表
+std::vector<int> MultiChoiceG::getSelectedInts() const {
+    std::vector<int> result;
+    for (auto id : mSelectedIDs) {
+        auto it = mINTValues.find(id);
+        if (it != mINTValues.end()) {
+            result.push_back(it->second);
+        }
+    }
+    return result;
+}
+
+/// @brief 获取指定ID的按键
+/// @param id 按键ID
+/// @return 按键
+View* MultiChoiceG::getView(int id) {
+    auto it = mButtons.find(id);
+    if (it != mButtons.end()) return it->second;
+    return nullptr;
+}
+
+/// @brief 获取按键组
+/// @return 按键组
+MultiChoiceG::ButtonViews* MultiChoiceG::getButtons() {
+    return &mButtons;
+}
+
+/// @brief 获取选中项数量
+/// @return 选中项数量
+size_t MultiChoiceG::getSelectedCount() const {
+    return mSelectedIDs.size();
+}
+
+/// @brief 按键点击事件
+/// @param v 按键
+void MultiChoiceG::onButtonClick(View& v) {
+    int id = v.getId();
+    bool oldSelected = isSelected(id);
+    bool newSelected = !oldSelected;
+
+    // 切换选中状态
+    toggleView(id);
+
+    // 回调监听器
+    if (mOnClickListener) mOnClickListener(id, newSelected);
 }
--- src/app/page/components/button_group.h
+++ src/app/page/components/button_group.h
@@ -2,7 +2,7 @@
  * @Author: xialc
  * @Email:
  * @Date: 2026-01-14 16:09:30
- * @LastEditTime: 2026-01-14 16:56:40
+ * @LastEditTime: 2026-01-22 22:47:51
  * @FilePath: /kk_frame/src/app/page/components/button_group.h
  * @Description: 按键组
  *               SingleChoice:单选  MultiChoice:多选
@@ -16,6 +16,7 @@
 #define __BUTTON_GROUP_H__
 
 #include <map>
+#include <set>
 #include <view/view.h>
 
 /// @brief 单选按键组
@@ -25,18 +26,22 @@ public:
     typedef std::map<int, const std::string> ButtonSTRValues;
     typedef std::map<int, int>               ButtonINTValues;
 
+    DECLARE_UIEVENT(bool, OnItemCLickListener, int oldID, int newID);
+
 public:
     SingleChoiceG();
     ~SingleChoiceG();
     bool addView(View* view);
     void setStrMap(const ButtonSTRValues& values);
     void setIntMap(const ButtonINTValues& values);
-    void setOnClickListener(View::OnClickListener l);
+    void setOnClickListener(OnItemCLickListener l);
     bool clickView(int id);
+
     int          getID();
     std::string  getString();
     int          getInt();
-    View*        get(int id);
+
+    View*        getView(int id);
     ButtonViews* getButtons();
 
 protected:
@@ -47,14 +52,50 @@ protected:
     std::map<int, View*>    mButtons;
     ButtonSTRValues         mSTRValues;
     ButtonINTValues         mINTValues;
-    View::OnClickListener   mOnClickListener;
+    OnItemCLickListener     mOnClickListener;
 };
 
 /// @brief 多选按键组 TODO
 class MultiChoiceG {
 public:
-    MultiChoiceG() = default;
-    ~MultiChoiceG() = default;
+    typedef std::map<int, View*>             ButtonViews;
+    typedef std::map<int, const std::string> ButtonSTRValues;
+    typedef std::map<int, int>               ButtonINTValues;
+    typedef std::set<int>                    SelectedSet;
+
+    DECLARE_UIEVENT(bool, OnItemCLickListener, int id, bool selected);
+
+public:
+    MultiChoiceG();
+    ~MultiChoiceG();
+
+    bool addView(View* view);
+    void setStrMap(const ButtonSTRValues& values);
+    void setIntMap(const ButtonINTValues& values);
+    void setOnClickListener(OnItemCLickListener l);
+
+    bool selectView(int id, bool selected = true);
+    bool toggleView(int id);
+    void clearSelection();
+
+    bool                     isSelected(int id) const;
+    SelectedSet              getSelectedIDs() const;
+    std::vector<std::string> getSelectedStrings() const;
+    std::vector<int>         getSelectedInts() const;
+
+    View*        getView(int id);
+    ButtonViews* getButtons();
+    size_t       getSelectedCount() const;
+
+protected:
+    virtual void onButtonClick(View& v);
+
+protected:
+    SelectedSet             mSelectedIDs;
+    std::map<int, View*>    mButtons;
+    ButtonSTRValues         mSTRValues;
+    ButtonINTValues         mINTValues;
+    OnItemCLickListener     mOnClickListener;
 };
 
 #endif // !__BUTTON_GROUP_H__
