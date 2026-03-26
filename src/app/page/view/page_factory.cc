8cc42d6beb30d042773f7d96129030981ba29087
Ricken <hackricken@gmail.com>
2026-03-26 19:13:38 +0800

    🔮 Product testing logic is complete

--- src/app/page/view/page_factory.cc
+++ src/app/page/view/page_factory.cc
@@ -2,7 +2,7 @@
  * @Author: Ricken
  * @Email: me@ricken.cn
  * @Date: 2026-03-22 12:32:26
- * @LastEditTime: 2026-03-22 12:39:12
+ * @LastEditTime: 2026-03-26 18:07:57
  * @FilePath: /kk_frame/src/app/page/view/page_factory.cc
  * @Description:
  * @BugList:
@@ -12,6 +12,7 @@
 **/
 
 #include "page_factory.h"
+#include "wind_mgr.h"
 
 PAGE_REGISTER(PAGE_FACTORY, PageFactory);
 
@@ -19,9 +20,64 @@ PageFactory::PageFactory() :PageBase("@layout/page_factory") {
     initUI();
 }
 
-PageFactory::~PageFactory() {
-}
+PageFactory::~PageFactory() { }
 
 int8_t PageFactory::getType() const {
     return PAGE_FACTORY;
 }
+
+void PageFactory::getView() {
+    mFlipper = __dc(ViewFlipper, mRootView);
+}
+
+void PageFactory::setView() {
+    setFactoryMenu();
+    setFactoryTouch();
+    setFactoryColor();
+    setFactorySwitch();
+}
+
+void PageFactory::onTick() { }
+
+void PageFactory::onAttach() {
+    mCurPage = FACTORY_MENU;
+    mFlipper->setDisplayedChild(mCurPage);
+}
+
+void PageFactory::setFactoryMenu() {
+    click(AppRid::to_touch, [this](View&) {
+        mCurPage = FACTORY_TOUCH;
+        mFlipper->setDisplayedChild(mCurPage);
+    });
+
+    click(AppRid::to_color, [this](View&) {
+        mCurPage = FACTORY_COLOR;
+        mFlipper->setDisplayedChild(mCurPage);
+        mFlipper->getChildAt(mCurPage)->getBackground()->setLevel(1);
+    });
+
+    click(AppRid::exit, [](View&) {
+        g_windMgr->goToPageBack();
+    });
+}
+
+void PageFactory::setFactoryTouch() {
+
+
+}
+
+void PageFactory::setFactoryColor() {
+    click(mFlipper->getChildAt(FACTORY_COLOR), [this](View& v) {
+        cdroid::Drawable* bg = v.getBackground();
+
+        int level = bg->getLevel();
+        if (level < 7) {
+            bg->setLevel(++level);
+        } else {
+            bg->setLevel(1);
+            mFlipper->setDisplayedChild(FACTORY_MENU);
+        }
+    });
+}
+
+void PageFactory::setFactorySwitch() { }
--- src/app/page/view/page_factory.h
+++ src/app/page/view/page_factory.h
@@ -2,7 +2,7 @@
  * @Author: Ricken
  * @Email: me@ricken.cn
  * @Date: 2026-03-22 12:32:19
- * @LastEditTime: 2026-03-22 12:34:20
+ * @LastEditTime: 2026-03-26 18:08:38
  * @FilePath: /kk_frame/src/app/page/view/page_factory.h
  * @Description: 工厂界面
  * @BugList:
@@ -15,13 +15,40 @@
 #define __PAGE_FACTORY_H__
 
 #include "page.h"
+#include <widget/viewflipper.h>
 
 class PageFactory :public PageBase {
+private:
+    typedef enum {
+        FACTORY_MENU = 0,  // 菜单页
+        FACTORY_TOUCH,     // 触摸测试页
+        FACTORY_COLOR,     // 颜色测试页
+        FACTORY_SWITCH,    // 开关测试页
+
+        FACTORY_MAX,       // 最大值
+    } FactoryPageType;
+
+private:
+    cdroid::ViewFlipper* mFlipper{ nullptr };
+    FactoryPageType mCurPage{ FACTORY_MENU };
+
 public:
     PageFactory();
     ~PageFactory();
 
     int8_t getType() const override;
+
+protected:
+    void getView() override;
+    void setView() override;
+    void onTick() override;
+    void onAttach() override;
+
+private:
+    void setFactoryMenu();
+    void setFactoryTouch();
+    void setFactoryColor();
+    void setFactorySwitch();
 };
 
 #endif // __PAGE_FACTORY_H__
