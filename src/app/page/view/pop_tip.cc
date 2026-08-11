5cc36e9f58b80aea324465950f7961e4b5d0694e
Ricken <hackricken@gmail.com>
2026-08-11 18:00:27 +0800

    ✨ Update Tip Pop

--- src/app/page/view/pop_tip.cc
+++ src/app/page/view/pop_tip.cc
@@ -2,7 +2,7 @@
  * @Author: Ricken
  * @Email: me@ricken.cn
  * @Date: 2025-02-13 16:26:20
- * @LastEditTime: 2025-12-29 14:25:58
+ * @LastEditTime: 2026-08-11 17:57:37
  * @FilePath: /kk_frame/src/app/page/view/pop_tip.cc
  * @Description:
  * @BugList:
@@ -16,15 +16,16 @@
 POP_REGISTER(POP_TIP, TipPop);
 
 TipPop::TipPop() :PopBase("@layout/pop_tip") {
+    setGauss();
     initUI();
 }
 
-TipPop::~TipPop() {
-}
+TipPop::~TipPop() { }
 
 int8_t TipPop::getType() const {
     return POP_TIP;
 }
 
 void TipPop::initUI() {
+    click(AppRid::btn_ok, [this](View&) {close();});
 }
